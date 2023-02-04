6e10e9f46c68bed44602e5293e843ec6071bc1b4
RikelmeBastosGomes00 <rikelmebastosgomes@gmail.com>
2023-02-04 12:39:42 -0300

    segunda aula

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { StatusBar,StyleSheet,SafeAreaView,View } from 'react-native';
+import react from 'react';
+import { useFonts,Montserrat_400Regular,Montserrat_700Bold }from '@expo-google-fonts/montserrat';
+import Cesta from './src/telas/Cesta';
+import { FontDisplay } from 'expo-font';
+
+export default function App(){
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+  if(!fonteCarregada){
+   return <View/> 
+  }
   return (
-    <View style={styles.container}>
-      <Text>Hello world!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView>
+     <StatusBar/>
+     <Cesta/>
+    </SafeAreaView>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
