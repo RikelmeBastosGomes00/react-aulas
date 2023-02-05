d18b1ba2d7abaa14eab02cbd74743dd1f576bfac
RikelmeBastosGomes00 <rikelmebastosgomes@gmail.com>
2023-02-05 13:08:50 -0300

    Quarta Aulau

--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {View,Image,StyleSheet } from "react-native";
+import {View,Image,StyleSheet,TouchableOpacity} from "react-native";
 
 
 import Texto from "../../../Componentes/Texto";
 
-export default function Detalhes ({nome,nomeFazenda,logoFazenda,descricao,preco}){
+export default function Detalhes ({nome,nomeFazenda,logoFazenda,descricao,preco,botao}){
     return<>
         <Texto style={estilos.nome}>{nome}</Texto>
     <View style={estilos.Fazenda}>
@@ -13,6 +13,10 @@ export default function Detalhes ({nome,nomeFazenda,logoFazenda,descricao,preco}
     </View>
     <Texto style={estilos.desc}>{descricao}</Texto>
      <Texto style={estilos.preco}>{preco}</Texto>
+
+     <TouchableOpacity style = {estilos.botao}>
+        <Texto style ={estilos.Textobotao}>{botao}</Texto>
+        </TouchableOpacity> 
     </>
 }
 const estilos =StyleSheet.create({
@@ -50,5 +54,19 @@ const estilos =StyleSheet.create({
         fontSize:26,
         lineHeight:42,
         marginTop:8
-        }
-});
+        },
+        botao:{
+        marginTop:16,
+        backgroundColor:"#2A9F85",
+        paddingVertical:16,
+        borderRadius:6
+        },
+        Textobotao:{
+        textAlign:"center",
+        color:"#fff",
+        fontSize:16,
+        lineHeight:26,
+        fontWeight:"bold",
+        },
+    
+})
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,18 +1,41 @@
 import react from "react";
-import { StyleSheet,View} from 'react-native';
+import {FlatList, StyleSheet,View} from 'react-native';
 import Detalhes from "./componentes/Detalhes";
 import Topo from'./componentes/Topo';
+import Item from "./componentes/Item";
+
 
 import { processFontFamily } from "expo-font";
-export default function Cesta({topo,detalhes}){
+import Texto from "../../Componentes/Texto";
+export default function Cesta({topo,detalhes,itens}){
 return<>
- <Topo {...topo}/>
- <View style={estilos.cesta}>
-    <Detalhes{...detalhes}/>
+
+<FlatList
+    data={itens.lista}
+    renderItem={Item}
+    keyExtractor={({nome}) => nome }
+    ListHeaderComponent={()=>{
+    return <>
+<Topo {...topo}/>
+<View style={estilos.cesta}>
+    <Detalhes {...detalhes}/>
+    <Texto style={estilos.titulo}>{itens.titulo}</Texto>
+ 
  </View>
+    </>
+    }}
+    />
  </>
 }
 const estilos =StyleSheet.create({
+    titulo:{
+        color:"46464646",
+        fontWeight:"bold",
+        marginTop:32,
+        marginBo000ttom:8,
+        fontSize:20,
+        lineHeight:32,
+      },
     cesta:{
         paddingVertical:8,
         paddingHorizontal:16,
